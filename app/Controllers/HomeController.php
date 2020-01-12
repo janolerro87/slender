f6adabb2642ed8f32a3a37c09426abb3acd3fee0
Javier Norberto <janolerro87@gmail.com>
2020-01-11 23:23:19 -0600

    Refactoring boostrap files and main controllers

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,20 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Controllers\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Views\Twig;
-use PDO;
 
-class HomeController
+class HomeController extends Controller
 {
-    protected $view;
-
-    public function __construct(Twig $view)
-    {
-        $this->view = $view;
-    }
-
     public function index(Request $request, Response $response)
     {
         return $this->view->render($response, 'home.twig');
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,48 @@
 <?php
 
-use Slim\Factory\AppFactory;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require 'container.php';
+$container = new \DI\Container();
 
-$app = AppFactory::create();
+\Slim\Factory\AppFactory::setContainer($container);
+$app = \Slim\Factory\AppFactory::create();
 
-require 'middleware.php';
+$container->set('settings', function () {
+    return [
+        'displayErrorDetails' => true,
+
+        'app' => [
+            'name' => 'App'
+        ],
+
+        'views' => [
+            'cache' => false
+        ]
+    ];
+});
+
+$twig = new Slim\Views\Twig(__DIR__ . '/../views', [
+    'cache' => $container->get('settings')['views']['cache']
+]);
+
+$container->set('view', $twig);
+
+$app->add(\Slim\Views\TwigMiddleware::createFromContainer($app));
+
+$errorMiddleware = new \Slim\Middleware\ErrorMiddleware(
+    $app->getCallableResolver(),
+    $app->getResponseFactory(),
+    true,
+    false,
+    false
+);
+
+$errorMiddleware->setErrorHandler(\Slim\Exception\HttpNotFoundException::class, function ($request, $exception) use ($container) {
+    $response = new \Slim\Psr7\Response();
+
+    return $container->get('view')->render($response->withStatus(404), 'errors/404.twig');
+});
+
+$app->add($errorMiddleware);
 
 require '../routes/web.php';
